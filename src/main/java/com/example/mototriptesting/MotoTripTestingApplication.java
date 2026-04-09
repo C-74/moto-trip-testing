c9dd5b8648de246ac6cb46355aabd65d1fb4ffbe
Yanis BELLAHCENE <bellahceneyanis03@gmail.com>
2026-04-09 13:43:14 +0200

    Initial commit

---
+++ src/main/java/com/example/mototriptesting/MotoTripTestingApplication.java
@@ -0,0 +1,13 @@
+package com.example.mototriptesting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MotoTripTestingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MotoTripTestingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/mototriptesting/MotoTripTestingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mototriptesting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MotoTripTestingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
